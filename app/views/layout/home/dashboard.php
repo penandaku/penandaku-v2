3a83db4884e4eac94b45c6faaede8a0db8140440
maulayyacyber <ridaulmaulayya@gmail.com>
2016-11-16 16:02:19 +0700

    test new ui

--- app/views/layout/home/dashboard.php
+++ app/views/layout/home/dashboard.php
@@ -1,10 +1,25 @@
+<div class="fb-profile">
+    <img align="left" class="fb-image-lg" src="https://www.socmatic.com/assets/templates/frontend/default/images/background/bg-header-2.jpg" alt="Profile image example"/>
+    <img align="left" class="fb-image-profile thumbnail" src="<?php echo cdn('img/fika.jpg') ?>" alt="Profile image example"/>
+      <div class="fb-profile-text">
+            <h1>Fika Ridaul maulayya</h1>
+            <p><i class="fa fa-clock-o"> Joined 20 - Sep - 2016</i></p>
+      </div>
+</div>
+<hr>
 <div class="container" style="margin-top:40px">
   <div class="row">
     <div class="col-md-3">
-      <div class="user-sidebar" style="position:fixed;top:70px;">
-        <div class="user-picture" style="border-radius: 75px;overflow: hidden;position: relative;">
-
-        </div>
+      <div class="card">
+          <div class="card-content">
+              <p style="font-weight: 300;font-size: 18px;">Cards for display in portfolio style material design by Google.</p>
+              <div class="date-bookmark" style="margin-top:10px">
+                <p><i class="fa fa-clock-o"> <i>20 - Sep - 2016</i></i></p>
+              </div>
+          </div>
+          <div class="card-action">
+              <a href="#" target="new_blank">Detail Bookmark <i class="fa fa-arrow-right"></i></a>
+          </div>
       </div>
     </div>
 

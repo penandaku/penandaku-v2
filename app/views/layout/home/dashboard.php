9844e283a3052028ad2c0e223f624375748c15e0
maulayyacyber <ridaulmaulayya@gmail.com>
2016-11-16 14:49:01 +0700

    dashboard home

--- app/views/layout/home/dashboard.php
+++ app/views/layout/home/dashboard.php
@@ -1,13 +1,51 @@
 <div class="container" style="margin-top:40px">
   <div class="row">
-    <div class="col-md-12">
-      <div class="panel panel-default">
-        <div class="panel-heading"><i class="fa fa-bookmark"></i> Last Bookmark</div>
-        <div class="panel-body">
+    <div class="col-md-3">
+      <div class="user-sidebar" style="position:fixed;top:70px;">
+        <div class="user-picture" style="border-radius: 75px;overflow: hidden;position: relative;">
 
         </div>
-        <div class="panel-footer" style="text-align:center">Show more bookmark...</div>
       </div>
+    </div>
+
+    <div class="col-md-9">
+          <div class="card">
+              <div class="card-content">
+                  <p style="font-weight: 300;font-size: 18px;">Cards for display in portfolio style material design by Google.</p>
+                  <div class="date-bookmark" style="margin-top:10px">
+                    <p><i class="fa fa-clock-o"> <i>20 - Sep - 2016</i></i></p>
+                  </div>
+              </div>
+              <div class="card-action">
+                  <a href="#" target="new_blank">Detail Bookmark <i class="fa fa-arrow-right"></i></a>
+              </div>
+          </div>
+
+          <div class="card">
+              <div class="card-content">
+                  <p style="font-weight: 300;font-size: 18px;">Cards for display in portfolio style material design by Google.</p>
+                  <div class="date-bookmark" style="margin-top:10px">
+                    <p><i class="fa fa-clock-o"> <i>20 - Sep - 2016</i></i></p>
+                  </div>
+              </div>
+              <div class="card-action">
+                  <a href="#" target="new_blank">Detail Bookmark <i class="fa fa-arrow-right"></i></a>
+              </div>
+          </div>
+
+          <div class="card">
+              <div class="card-content">
+                  <p style="font-weight: 300;font-size: 18px;">Cards for display in portfolio style material design by Google.</p>
+                  <div class="date-bookmark" style="margin-top:10px">
+                    <p><i class="fa fa-clock-o"> <i>20 - Sep - 2016</i></i></p>
+                  </div>
+              </div>
+              <div class="card-action">
+                  <a href="#" target="new_blank">Detail Bookmark <i class="fa fa-arrow-right"></i></a>
+              </div>
+          </div>
+
+
     </div>
   </div>
 </div>

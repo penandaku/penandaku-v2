7e5d0e7315e937aca69ccc3f9144b47191979ee6
kesatrianglungupertama@gmail.com <kesatrianglungupertama@gmail.com>
2016-11-21 10:20:54 +0700

    sign up button

--- app/views/part/header.php
+++ app/views/part/header.php
@@ -49,7 +49,8 @@
           <?php }else{ ?>
               <div  class="navbar-form navbar-right" style = 'margin-top:13px;'>
                 <a href="<?php print base_url() ?>login?source=header&penandaku_token=<?php echo $this->security->get_csrf_hash(); ?>&utf8=✓" class="btn btn-default" style="padding: 9px 15px;font-family:Roboto;font-weight:normal"><i class="fa fa-sign-in"></i> Sign in</a>
-                <a href="<?php print base_url() ?>join?source=header&penandaku_token=<?php echo $this->security->get_csrf_hash(); ?>&utf8=✓" class="btn btn-success" style="padding: 9px 15px;font-family:Roboto;font-weight:normal"><i class="fa fa-user-plus"></i> Sign up</a>
+                <?php if(!$this->uri->segment(1)=="join"){ ?>
+				<a href="<?php print base_url() ?>join?source=header&penandaku_token=<?php echo $this->security->get_csrf_hash(); ?>&utf8=✓" class="btn btn-success" style="padding: 9px 15px;font-family:Roboto;font-weight:normal"><i class="fa fa-user-plus"></i> Sign up</a><?php } ?>
               </div>
          <?php } ?>
         </div>
